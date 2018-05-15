3db974d46bf71ad084b1f24bfd2de57b293d1cda
IvayloAsenov <ivaylo.asenov@hotmail.com>
2018-05-15 17:21:00 -0400

    Worked on ChooseGoalsActivity

--- app/src/main/java/com/example/ivo/workoutbuilderai/ChooseGoalsActivity.java
+++ app/src/main/java/com/example/ivo/workoutbuilderai/ChooseGoalsActivity.java
@@ -6,13 +6,33 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
+/**
+ * Activity that lets the user pick his goals when it
+ * comes to his workout. The user can either pick one, two, or
+ * all three of them.
+ *
+ * @author Ivaylo Asenov
+ */
 public class ChooseGoalsActivity extends AppCompatActivity {
+
+    ImageView iv_textChooseGoals;
+    ImageView iv_loseWeight;
+    ImageView iv_buildMuscle;
+    ImageView iv_increaseEndurance;
+    ImageView iv_continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_goals);
+
+        iv_textChooseGoals = findViewById(R.id.iv_textChooseGoals);
+        iv_loseWeight = findViewById(R.id.iv_loseWeight);
+        iv_buildMuscle = findViewById(R.id.iv_buildMuscle);
+        iv_increaseEndurance = findViewById(R.id.iv_increaseEndurance);
+        iv_continue = findViewById(R.id.iv_continueGoals);
 
     }
 
--- app/src/main/java/com/example/ivo/workoutbuilderai/HomeActivity.java
+++ app/src/main/java/com/example/ivo/workoutbuilderai/HomeActivity.java
@@ -2,12 +2,26 @@ package com.example.ivo.workoutbuilderai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+/**
+ * Activity that acts as home screen from where the user can
+ * build a new workout or to see previous workouts
+ *
+ * @author Ivaylo Asenov
+ * */
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageView iv_buildWorkout;
+    ImageView iv_seeWorkouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        iv_buildWorkout = findViewById(R.id.iv_buildWorkout);
+        iv_seeWorkouts  = findViewById(R.id.iv_seeWorkouts);
     }
 }
